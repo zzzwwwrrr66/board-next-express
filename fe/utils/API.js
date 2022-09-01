ea1a2df55451950872c3157c9d7cafbcbedbc269
wooram <zzzwwwrrr66@gmail.com>
2022-09-02 02:00:51 +0900

    update API url

--- fe/utils/API.js
+++ fe/utils/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.nodebird.com' : 'http://localhost:8080';
 
 const API = axios.create({
-	BASE_URL: 'http://localhost:8080',
+	BASE_URL: backUrl,
   headers: {
       'Content-Type': 'application/json',
   },
